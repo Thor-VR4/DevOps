2d52aec1286b0666786657be0109509c0ee3cfcf
Thor_VR4 <blackthor@yandex.ru>
2021-09-29 17:53:18 +0300

    Add branch cloud-testapp

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yc compute instance create \
+  --name reddit-app2 \
+  --hostname reddit-app2 \
+  --memory=4 \
+  --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1604-lts,size=10GB \
+  --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
+  --core-fraction=5 \
+  --metadata serial-port-enable=1 \
+  --metadata-from-file user-data=metadata.yml
