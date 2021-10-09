f60568e115bd70d0b94ab25e685324785c1371d0
Thor_VR4 <blackthor@yandex.ru>
2021-10-09 18:10:18 +0300

    Fixed your old python's error

--- ansible/my_yc_instance2.py
+++ ansible/my_yc_instance2.py
@@ -3,7 +3,6 @@
 import json
 import subprocess
 import argparse
-import re
 
 class MyInventory(object):
 
@@ -38,7 +37,7 @@ class MyInventory(object):
         '''Populate inventory with given instances'''
         cmd2 = "(cd ../terraform/stage/ && terraform output -json)"
         output = subprocess.run(cmd2, stdout=subprocess.PIPE, shell=True)
-        yc_obj = json.loads(output.stdout)
+        yc_obj = json.loads(output.stdout.decode('utf-8'))
         ip_db = yc_obj.get ('external_ip_address_db',()).get ('value',())
         ip_app = yc_obj.get ('external_ip_address_app',()).get ('value',())
         return {
